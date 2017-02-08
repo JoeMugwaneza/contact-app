816f87d4ed1f627529d154cd63e905dfbd886227
JoeMugwaneza <muganezajoseph@gmail.com>
2017-02-08 09:33:44 +0200

    worked on index and show page

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,5 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
-  "/contacts", to: 'contacts#index'
+  get "/contacts", to: 'contacts#index'
+  get "/contacts/:id", to: 'contacts#show'
 end
